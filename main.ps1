77d3ac2258d162816c90c5b0cffa57f19006340d
Lionel <massin.lionel@hotmail.com>
2025-07-08 15:31:35 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -13,7 +13,7 @@ if (-not (Get-Command ConvertFrom-Yaml -ErrorAction SilentlyContinue)) {
             [System.Windows.Forms.MessageBoxButtons]::OK,
             [System.Windows.Forms.MessageBoxIcon]::Error
         )
-        exit
+        exit 1
     }
 }
 
@@ -35,6 +35,42 @@ $logFile = Join-Path $logDir "$user`_$date.txt"
 $configMenu      = Get-Content $configMenuFile      -Raw -Encoding UTF8 | ConvertFrom-Yaml
 $configInterface = Get-Content $configInterfaceFile -Raw -Encoding UTF8 | ConvertFrom-Yaml
 
+# --- Validation des configurations YAML ---
+function Validate-Config {
+    param(
+        [hashtable]$Config,
+        [string[]] $RequiredKeys,
+        [string]   $ConfigName
+    )
+    foreach ($key in $RequiredKeys) {
+        if (-not $Config.ContainsKey($key)) {
+            [System.Windows.Forms.MessageBox]::Show(
+                "La clé '$key' est manquante dans $ConfigName.",
+                "Erreur de configuration",
+                [System.Windows.Forms.MessageBoxButtons]::OK,
+                [System.Windows.Forms.MessageBoxIcon]::Error
+            )
+            exit 1
+        }
+    }
+}
+
+# Valider les clés essentielles
+Validate-Config -Config $configMenu      -RequiredKeys @('MenuOptions')        -ConfigName 'config_menu.yaml'
+Validate-Config -Config $configInterface -RequiredKeys @('Interface')          -ConfigName 'config_interface.yaml'
+
+# Vérifier que MenuOptions est une collection
+if (-not ($configMenu.MenuOptions -is [System.Collections.IEnumerable])) {
+    [System.Windows.Forms.MessageBox]::Show(
+        "config_menu.yaml : 'MenuOptions' doit être une liste.",
+        "Erreur de configuration",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Error
+    )
+    exit 1
+}
+
+# --- Préparer les objets pour l'UI ---
 $MenuOptions = $configMenu.MenuOptions
 $UI          = $configInterface.Interface
 
@@ -42,90 +78,107 @@ $UI          = $configInterface.Interface
 Get-ChildItem $optionsDir -Filter '*.ps1' | ForEach-Object { . $_.FullName }
 
 # --- Construire l'interface ---
-$form = New-Object System.Windows.Forms.Form
-$form.Text          = $UI.Title
-$form.Size          = New-Object System.Drawing.Size($UI.Size.Width, $UI.Size.Height)
-$form.StartPosition = $UI.StartPosition
+function Build-Form {
+    param($UI, $MenuOptions)
 
-# ListBox
-$listBox = New-Object System.Windows.Forms.ListBox
-$listBox.Location   = New-Object System.Drawing.Point($UI.ListBox.Location.X, $UI.ListBox.Location.Y)
-$listBox.Size       = New-Object System.Drawing.Size($UI.ListBox.Size.Width,     $UI.ListBox.Size.Height)
-$listBox.DataSource = $MenuOptions.label
-$form.Controls.Add($listBox)
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text          = $UI.Title
+    $form.Size          = New-Object System.Drawing.Size($UI.Size.Width, $UI.Size.Height)
+    $form.StartPosition = $UI.StartPosition
 
-# TextBox de logs
-$txtLog = New-Object System.Windows.Forms.TextBox
-$txtLog.Multiline   = $true
-$txtLog.ReadOnly    = $true
-$txtLog.ScrollBars  = 'Vertical'
-$txtLog.Location    = New-Object System.Drawing.Point($UI.LogTextBox.Location.X, $UI.LogTextBox.Location.Y)
-$txtLog.Size        = New-Object System.Drawing.Size($UI.LogTextBox.Size.Width,     $UI.LogTextBox.Size.Height)
-$form.Controls.Add($txtLog)
+    # ListBox
+    $listBox = New-Object System.Windows.Forms.ListBox
+    $listBox.Name       = 'ListBox_Options'
+    $listBox.Location   = New-Object System.Drawing.Point($UI.ListBox.Location.X, $UI.ListBox.Location.Y)
+    $listBox.Size       = New-Object System.Drawing.Size($UI.ListBox.Size.Width, $UI.ListBox.Size.Height)
+    $listBox.DataSource = $MenuOptions.label
+    $form.Controls.Add($listBox)
 
-# ProgressBar
-$pb = New-Object System.Windows.Forms.ProgressBar
-$pb.Location  = New-Object System.Drawing.Point($UI.ProgressBar.Location.X, $UI.ProgressBar.Location.Y)
-$pb.Size      = New-Object System.Drawing.Size($UI.ProgressBar.Size.Width,     $UI.ProgressBar.Size.Height)
-$pb.Minimum   = $UI.ProgressBar.Minimum
-$pb.Maximum   = $UI.ProgressBar.Maximum
-$form.Controls.Add($pb)
+    # TextBox de logs
+    $txtLog = New-Object System.Windows.Forms.TextBox
+    $txtLog.Name       = 'TextBox_Log'
+    $txtLog.Multiline   = $true
+    $txtLog.ReadOnly    = $true
+    $txtLog.ScrollBars  = 'Vertical'
+    $txtLog.Location    = New-Object System.Drawing.Point($UI.LogTextBox.Location.X, $UI.LogTextBox.Location.Y)
+    $txtLog.Size        = New-Object System.Drawing.Size($UI.LogTextBox.Size.Width, $UI.LogTextBox.Size.Height)
+    $form.Controls.Add($txtLog)
 
-# Fonction de log partagée
+    # ProgressBar
+    $pb = New-Object System.Windows.Forms.ProgressBar
+    $pb.Name      = 'ProgressBar_Main'
+    $pb.Location  = New-Object System.Drawing.Point($UI.ProgressBar.Location.X, $UI.ProgressBar.Location.Y)
+    $pb.Size      = New-Object System.Drawing.Size($UI.ProgressBar.Size.Width, $UI.ProgressBar.Size.Height)
+    $pb.Minimum   = $UI.ProgressBar.Minimum
+    $pb.Maximum   = $UI.ProgressBar.Maximum
+    $form.Controls.Add($pb)
+
+    # Bouton Exécuter
+    $btnRun = New-Object System.Windows.Forms.Button
+    $btnRun.Name     = 'Button_Run'
+    $btnRun.Text     = $UI.Buttons.Run.Text
+    $btnRun.Size     = New-Object System.Drawing.Size($UI.Buttons.Run.Size.Width, $UI.Buttons.Run.Size.Height)
+    $btnRun.Location = New-Object System.Drawing.Point($UI.Buttons.Run.Location.X, $UI.Buttons.Run.Location.Y)
+    $btnRun.Add_Click({
+        Write-Log "Lancement de l'option sélectionnée"
+        $idx = $form.Controls['ListBox_Options'].SelectedIndex
+        if ($idx -lt 0) {
+            Write-Log 'Erreur : aucune option sélectionnée'
+            return
+        }
+        $opt = $MenuOptions[$idx]
+        Write-Log "-> Fonction : $($opt.function)"
+        $pb.Value = 0
+
+        # Préparer splatting
+        $splat = @{}
+        foreach ($k in $opt.params.Keys) { $splat[$k] = $opt.params[$k] }
+
+        switch ($opt.function) {
+            'Clear-DiskSpace' {
+                $pb.Maximum = 4
+                & $opt.function
+            }
+            'Compress-Images' {
+                $dlg = [System.Windows.Forms.FolderBrowserDialog]::new()
+                if ($dlg.ShowDialog() -eq 'OK') {
+                    $splat['FolderPath'] = $dlg.SelectedPath
+                    & $opt.function @splat
+                }
+            }
+            default {
+                & $opt.function @splat
+            }
+        }
+
+        Write-Log "Terminé : $($opt.function)"
+    })
+    $form.Controls.Add($btnRun)
+
+    # Bouton Quitter
+    $btnExit = New-Object System.Windows.Forms.Button
+    $btnExit.Name     = 'Button_Exit'
+    $btnExit.Text     = $UI.Buttons.Exit.Text
+    $btnExit.Size     = New-Object System.Drawing.Size($UI.Buttons.Exit.Size.Width, $UI.Buttons.Exit.Size.Height)
+    $btnExit.Location = New-Object System.Drawing.Point($UI.Buttons.Exit.Location.X, $UI.Buttons.Exit.Location.Y)
+    $btnExit.Add_Click({ $form.Close() })
+    $form.Controls.Add($btnExit)
+
+    return $form
+}
+
+# --- Fonction de log partagée ---
 function Write-Log {
     param([string]$message)
     $time  = (Get-Date).ToString('HH:mm:ss')
     $entry = "[$time] $message"
+    $form = [System.Windows.Forms.Application]::OpenForms | Where-Object { $_.Name -eq $UI.Title }
+    $txtLog = $form.Controls['TextBox_Log']
     $txtLog.AppendText($entry + "`r`n")
     Add-Content -Path $logFile -Value $entry
     [System.Windows.Forms.Application]::DoEvents()
 }
 
-# Bouton Exécuter
-$btnRun = New-Object System.Windows.Forms.Button
-$btnRun.Text     = $UI.Buttons.Run.Text
-$btnRun.Size     = New-Object System.Drawing.Size($UI.Buttons.Run.Size.Width,  $UI.Buttons.Run.Size.Height)
-$btnRun.Location = New-Object System.Drawing.Point($UI.Buttons.Run.Location.X, $UI.Buttons.Run.Location.Y)
-$btnRun.Add_Click({
-    $idx = $listBox.SelectedIndex
-    if ($idx -lt 0) { Write-Log 'Erreur : aucune option sélectionnée'; return }
-    $opt = $MenuOptions[$idx]
-
-    Write-Log "Lancement : $($opt.function)"
-    $pb.Value = 0
-
-    # Préparer splatting
-    $splat = @{}
-    foreach ($k in $opt.params.Keys) { $splat[$k] = $opt.params[$k] }
-
-    switch ($opt.function) {
-        'Clear-DiskSpace' {
-            $pb.Maximum = 4
-            & $opt.function
-        }
-        'Compress-Images' {
-            $dlg = [System.Windows.Forms.FolderBrowserDialog]::new()
-            if ($dlg.ShowDialog() -eq 'OK') {
-                $splat['FolderPath'] = $dlg.SelectedPath
-                & $opt.function @splat
-            }
-        }
-        default {
-            & $opt.function @splat
-        }
-    }
-
-    Write-Log "Terminé : $($opt.function)"
-})
-$form.Controls.Add($btnRun)
-
-# Bouton Quitter
-$btnExit = New-Object System.Windows.Forms.Button
-$btnExit.Text     = $UI.Buttons.Exit.Text
-$btnExit.Size     = New-Object System.Drawing.Size($UI.Buttons.Exit.Size.Width,  $UI.Buttons.Exit.Size.Height)
-$btnExit.Location = New-Object System.Drawing.Point($UI.Buttons.Exit.Location.X, $UI.Buttons.Exit.Location.Y)
-$btnExit.Add_Click({ $form.Close() })
-$form.Controls.Add($btnExit)
-
 # --- Affichage de la GUI ---
+$form = Build-Form -UI $UI -MenuOptions $MenuOptions
 [void]$form.ShowDialog()
